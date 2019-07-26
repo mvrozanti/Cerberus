54281abba932f2499aa4602e268d64c6d0846d3a
Marcelo <mvrozanti@hotmail.com>
2019-07-26 17:40:33 -0300

    delegar getOperacao bem sucedido

--- src/main/java/yet/another/test/spring/fucking/boot/Application.java
+++ src/main/java/yet/another/test/spring/fucking/boot/Application.java
@@ -1,10 +1,8 @@
 package yet.another.test.spring.fucking.boot;
 
-import javax.net.ssl.SSLContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/yet/another/test/spring/fucking/boot/ProtectedController.java
+++ src/main/java/yet/another/test/spring/fucking/boot/ProtectedController.java
@@ -2,13 +2,15 @@ package yet.another.test.spring.fucking.boot;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import s4desk.operacao.controller.ControllerEmprestimo;
+import s4desk.operacao.model.Operacao;
+import s4desk.operacao.model.dao.OperacaoDAO;
 import s4utils.ConfigUtils;
 import s4utils.DataBaseUtils;
 
@@ -18,19 +20,7 @@ public class ProtectedController {
 
 	private ControllerEmprestimo ce;
 
-	/**
-	 * This is an example of some different kinds of granular restriction for
-	 * endpoints. You can use the built-in SPEL expressions in @PreAuthorize
-	 * such as 'hasRole()' to determine if a user has access. However, if you
-	 * require logic beyond the methods Spring provides then you can encapsulate
-	 * it in a service and register it as a bean to use it within the annotation
-	 * as demonstrated below with 'securityService'.
-	 *
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-//  @PreAuthorize("hasRole('ADMIN')")
-	@PreAuthorize("@securityService.hasProtectedAccess()")
-	public ResponseEntity<?> getDaHoney() {
+	private void init(){
 		if (!ConfigUtils.foiLida()) {
 			ConfigUtils.readArguments(new String[]{});
 			try {
@@ -41,7 +31,35 @@ public class ProtectedController {
 				Logger.getLogger(ProtectedController.class.getName()).log(Level.SEVERE, null, ex);
 			}
 		}
+	}
+
+	/**
+	 * This is an example of some different kinds of granular restriction for
+	 * endpoints. You can use the built-in SPEL expressions in @PreAuthorize
+	 * such as 'hasRole()' to determine if a user has access. However, if you
+	 * require logic beyond the methods Spring provides then you can encapsulate
+	 * it in a service and register it as a bean to use it within the annotation
+	 * as demonstrated below with 'securityService'.
+	 *
+	 */
+	@RequestMapping(value = "/possiveis_empresas", method = RequestMethod.GET)
+	@PreAuthorize("@securityService.hasProtectedAccess()")
+	public ResponseEntity<?> getDaHoney() {
+		init();
 		return ResponseEntity.ok(ce.getPossiveisEmpresas());
+	}
+	@RequestMapping(value = "/operacoes/{idt}", method = RequestMethod.GET)
+	@PreAuthorize("@securityService.hasProtectedAccess()")
+	public ResponseEntity<?> getOperacao(@PathVariable("idt") long idt) {
+		init();
+		Operacao o = new Operacao(idt);
+		try {
+			new OperacaoDAO().getUnique(o);
+		} catch (Exception ex) {
+			Logger.getLogger(ProtectedController.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		ResponseEntity<?> re = ResponseEntity.ok(o);
+		return re;
 	}
 
 }
